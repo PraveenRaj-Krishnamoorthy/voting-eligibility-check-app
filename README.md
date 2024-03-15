e1b97e092fac691e287e05fc146a5dd9f16009fa
PraveenRaj-Krishnamoorthy <praveenraj.krishnamoorthy@outlook.com>
2024-03-15 21:03:11 +0530

    Summary(index & js Updated)
    Description(html and js are been updated and coding is clear!)

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # voting eligibility check app
  this is a simple voting eligibility check app description
 html, js, css fiels added
+(new readme updated testing!)
--- index.js
+++ index.js
@@ -1,7 +1,39 @@
-const func =()=>{
+class classes {
+    constructor(name, voter, age) {
+        this.n = name;
+        this.v = voter;
+        this.a = age;
+    }
+    condition() {
+        if (this.a >=18) {
+            this.e = true
+            if (this.v == "yes") {
+                true
+            } else {
+                false
+                alert("Apply Voter ID!")
+            }
+        } else {
+            alert("Below 18!")
+            this.e = false
+        }
+    }
+    display() {
+        let output = document.querySelector(".show");
+        output.innerHTML =
+            `
+    Name : ${this.n}<br>
+    Elidgibility : ${this.v == true && this.a==true ? "Elidgible" : "Not elidgible"}<br>
+    Age : ${this.e == true && this.a == true ? this.a : "Not Eligible!"}
+    `}
+}
+
+const func = () => {
     let form = document.forms["formdata"];
-    
+    let user = form["user"].value;
     let voter = form["r1"].value;
-    
-    console.log(voter);
+    let age = form["age"].value;
+    let vote = new classes(user, voter, age);
+    vote.condition();
+    vote.display();
 }
